cf434d0fff9f55e9b843e61bb63ae47dea0a2d22
Andrew Cox <drew.r.cox@gmail.com>
2017-05-01 14:52:59 -0400

    Update all endpoints to function properly and modularize routes with express router

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-const {BlogPosts} = require('./models');
-
-const jsonParser = bodyParser.json();
 const app = express();
+
+const blogPostsRouter = require('./blogPostsRouter');
 
 // log the http layer
 app.use(morgan('common'));
+
+app.use('/blog-posts', blogPostsRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
